975b078f43f69332e49a70b121e07e6b1d7c1f30
Daksh Nimavat <143920756+Dakshnimavat@users.noreply.github.com>
2024-10-19 08:55:54 +0000

    concersion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33ff"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let sumNumber = 33
+let stringNumber = String(sumNumber)
+console.log(typeof stringNumber);
+
