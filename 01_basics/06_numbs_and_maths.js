7240aaff25d917651384f11b16dbcba9a1617be0
Daksh Nimavat <143920756+Dakshnimavat@users.noreply.github.com>
2024-10-21 14:38:39 +0000

    maths

---
+++ 01_basics/06_numbs_and_maths.js
@@ -0,0 +1,26 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const Num = 233.544
+// console.log(Num.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ********************** maths ************************
+// console.log(Math.abs(-6));
+// console.log(Math.round(5.7));
+
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
